4cc510ff545a438cc7f61006d2088df8bfcde496
Hiko1234 <andriym1404@gmail.com>
2022-10-24 19:30:14 +0300

    first commit

---
+++ Project2/Source.cpp
@@ -0,0 +1,27 @@
+// Lab_02.cpp
+// < Мицько Андрій >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 14
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	double z2;
+
+	cout << "a = "; cin >> a;
+
+	z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+	z2 = tan(2 * a) + 1 / cos(2 * a);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
